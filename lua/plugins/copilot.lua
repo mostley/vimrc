ed1e345fa96a34cb77605c0a56a04c21ba6f41f7
Sven Hecht <sven@understand.ai>
2023-06-06 19:32:44 +0200

    disable copilot on startup

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,6 +1,4 @@
 return {
   "github/copilot.vim",
-  config = function()
-    -- vim.g.copilot_node_command = "~/.nvm/versions/node/v16.13.0/bin/node"
-  end,
+  cmd = "Copilot", -- disable loading on startup
 }
