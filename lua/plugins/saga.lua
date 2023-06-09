4dcf58e55340677a13ae81db0bf57620b4935626
Sven Hecht <sven@understand.ai>
2023-06-09 10:09:24 +0200

    add sane default for saga finder

--- lua/plugins/saga.lua
+++ lua/plugins/saga.lua
@@ -17,5 +17,20 @@ return {
       split = "<C-i>",
       quit = "q",
     },
+    finder = {
+      max_height = 0.5,
+      min_width = 30,
+      force_max_height = false,
+      keys = {
+        jump_to = "p",
+        expand_or_jump = { "o", "<enter>" },
+        vsplit = "<C-v>",
+        split = "<C-i>",
+        tabe = "t",
+        tabnew = "r",
+        quit = { "q", "<ESC>" },
+        close_in_preview = "<ESC>",
+      },
+    },
   },
 }
